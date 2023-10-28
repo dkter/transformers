48ebb0465f578910f0c333be2593ac1856bfda46
David Teresi <dkteresi@gmail.com>
2023-10-28 13:54:59 -0400

    restart button

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod map;
 
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
-use player::{spawn_player, move_player};
+use player::{Player, spawn_player, move_player};
 use map::spawn_map;
 
 fn setup(
@@ -14,10 +14,23 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 }
 
+fn check_restart(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    player_entities: Query<Entity, With<Player>>,
+) {
+    if keyboard_input.just_released(KeyCode::R) {
+        for entity in &player_entities {
+            commands.entity(entity).despawn();
+        }
+        spawn_player(commands);
+    }
+}
+
 fn main() {
     App::new()
         .add_systems(Startup, (setup, spawn_player, spawn_map))
-        .add_systems(Update, (move_player, player::set_jumping_false))
+        .add_systems(Update, (move_player, player::set_jumping_false, check_restart))
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .run();
